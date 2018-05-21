529814754208efc6a0aec8549d060fbc09011a8f
alfonsserra <alfonsoserra@gmail.com>
2018-05-21 07:15:25 +0200

    Move logic to a new service

--- src/main/java/com/systelab/inventory/controller/ApplicationController.java
+++ src/main/java/com/systelab/inventory/controller/ApplicationController.java
@@ -1,13 +1,6 @@
 package com.systelab.inventory.controller;
 
-import com.systelab.inventory.model.Product;
-import com.systelab.inventory.model.ProductGroup;
-import com.systelab.inventory.model.Supplier;
-import com.systelab.inventory.model.Warehouse;
-import com.systelab.inventory.repository.ProductGroupRepository;
-import com.systelab.inventory.repository.ProductRepository;
-import com.systelab.inventory.repository.SupplierRepository;
-import com.systelab.inventory.repository.WarehouseRepository;
+import com.systelab.inventory.service.ApplicationService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,59 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApplicationController {
 
     @Autowired
-    private WarehouseRepository warehouseRepository;
-
-    @Autowired
-    private SupplierRepository supplierRepository;
-
-    @Autowired
-    private ProductGroupRepository productGroupRepository;
-
-    @Autowired
-    private ProductRepository productRepository;
+    private ApplicationService applicationService;
 
     @ApiOperation(value = "Init some data", notes = "Initialize the DB with basic information in order to test the API.")
     @PostMapping("init")
     public boolean initSomeData() {
-        Warehouse warehouse1 = new Warehouse();
-        warehouse1.setName("Rotterdam");
-        warehouseRepository.save(warehouse1);
-
-        Warehouse warehouse2 = new Warehouse();
-        warehouse2.setName("Hong Kong");
-        Warehouse hk = warehouseRepository.save(warehouse2);
-
-        Supplier supplier1 = new Supplier();
-        supplier1.setName("Amazon");
-        supplierRepository.save(supplier1);
-
-        Supplier supplier2 = new Supplier();
-        supplier2.setName("Microsoft");
-        supplierRepository.save(supplier2);
-
-        Supplier supplier3 = new Supplier();
-        supplier3.setName("Apple");
-        Supplier apple = supplierRepository.save(supplier3);
-
-        ProductGroup productgroup1 = new ProductGroup();
-        productgroup1.setName("Books");
-        productGroupRepository.save(productgroup1);
-
-        ProductGroup productgroup2 = new ProductGroup();
-        productgroup2.setName("Computers");
-        ProductGroup computers = productGroupRepository.save(productgroup2);
-
-        Product product1 = new Product();
-        product1.setName("MacBook Pro");
-        product1.setReportDescription("MacBook Pro");
-        product1.setCode("435242342");
-        product1.setSupplierProductCode("345323423432");
-        product1.setSupplier(apple);
-        product1.setWarehouse(hk);
-        product1.setProductGroup(computers);
-        Product macbook = productRepository.save(product1);
-
-        return true;
+        return applicationService.initSomeData();
     }
 
 }
---
+++ src/main/java/com/systelab/inventory/service/ApplicationService.java
@@ -0,0 +1,72 @@
+package com.systelab.inventory.service;
+
+import com.systelab.inventory.model.Product;
+import com.systelab.inventory.model.ProductGroup;
+import com.systelab.inventory.model.Supplier;
+import com.systelab.inventory.model.Warehouse;
+import com.systelab.inventory.repository.ProductGroupRepository;
+import com.systelab.inventory.repository.ProductRepository;
+import com.systelab.inventory.repository.SupplierRepository;
+import com.systelab.inventory.repository.WarehouseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class ApplicationService {
+
+    @Autowired
+    private WarehouseRepository warehouseRepository;
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
+    @Autowired
+    private ProductGroupRepository productGroupRepository;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public boolean initSomeData() {
+        Warehouse warehouse1 = new Warehouse();
+        warehouse1.setName("Rotterdam");
+        warehouseRepository.save(warehouse1);
+
+        Warehouse warehouse2 = new Warehouse();
+        warehouse2.setName("Hong Kong");
+        Warehouse hk = warehouseRepository.save(warehouse2);
+
+        Supplier supplier1 = new Supplier();
+        supplier1.setName("Amazon");
+        supplierRepository.save(supplier1);
+
+        Supplier supplier2 = new Supplier();
+        supplier2.setName("Microsoft");
+        supplierRepository.save(supplier2);
+
+        Supplier supplier3 = new Supplier();
+        supplier3.setName("Apple");
+        Supplier apple = supplierRepository.save(supplier3);
+
+        ProductGroup productgroup1 = new ProductGroup();
+        productgroup1.setName("Books");
+        productGroupRepository.save(productgroup1);
+
+        ProductGroup productgroup2 = new ProductGroup();
+        productgroup2.setName("Computers");
+        ProductGroup computers = productGroupRepository.save(productgroup2);
+
+        Product product1 = new Product();
+        product1.setName("MacBook Pro");
+        product1.setReportDescription("MacBook Pro");
+        product1.setCode("435242342");
+        product1.setSupplierProductCode("345323423432");
+        product1.setSupplier(apple);
+        product1.setWarehouse(hk);
+        product1.setProductGroup(computers);
+        Product macbook = productRepository.save(product1);
+
+        return true;
+    }
+}
